d823e8c931690edef7cb5a006f39f75ac7d0b049
ZAZAZakari <tvbatvtv2@hotmail.com>
2016-08-27 06:40:56 +0000

    feat: a neural network for character classification
    Here commited a neural network for character classification,
    the job is to classifly an 32x32 image to each number (0~9)

--- characterClassification_train.py
+++ characterClassification_train.py
@@ -1,25 +1,25 @@
 import numpy as np
-import scipy.io
 import tensorflow as tf
+import scipy.io
 import freeze_graph
 import os
-trainMat = scipy.io.loadmat('processed_data/characterDetection/train.mat')
-testMat = scipy.io.loadmat('processed_data/characterDetection/test.mat')
+
+trainMat = scipy.io.loadmat('processed_data/characterRecognition/train.mat')
+validateMat = scipy.io.loadmat('processed_data/characterRecognition/validate.mat')
 
 trainingData = trainMat['data']
+validateData = validateMat['data']
 trainingLabel = trainMat['label']
-validateData = testMat['data']
-validateLabel = testMat['label']
-del trainMat
-del testMat
+validateLabel = validateMat['label']
 
+del trainMat
+del validateMat
 graph = tf.Graph()
 with graph.as_default():
     # Declaring placeholders 
     tfTrainingData = tf.placeholder(tf.float32, [None, 32, 32, 3], name='tfTrainingData')
-    tfTrainingLabels = tf.placeholder(tf.float32, [None, 2], name='tfTrainingLabels')
+    tfTrainingLabels = tf.placeholder(tf.float32, [None, 10], name='tfTrainingLabels')
     keep_prob = tf.placeholder(tf.float32, name='keep_prob')
-    
     def weight_variable(shape):
         return tf.Variable(tf.truncated_normal(shape, stddev=0.1))
     
@@ -61,44 +61,47 @@ with graph.as_default():
     h_fc2 = tf.nn.relu(tf.matmul(h_fc_1_drop, W_fc2) + b_fc2)
     h_fc_2_drop = tf.nn.dropout(h_fc2, keep_prob)
     
-    W_fc3 = weight_variable([1024, 2])
-    b_fc3 = bias_variable([2])  
+    W_fc3 = weight_variable([1024, 10])
+    b_fc3 = bias_variable([10])  
     logits = (tf.matmul(h_fc_2_drop, W_fc3) + b_fc3)
-    
     with tf.name_scope('Output'):
         output = tf.nn.softmax(logits, name='softmax')
         loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, tfTrainingLabels), name='loss')
         correctPrediction = tf.equal(tf.argmax(output, 1), tf.argmax(tfTrainingLabels, 1))
         accuracy = tf.reduce_mean(tf.cast(correctPrediction, tf.float32), name='accuracy')
     optimizer = tf.train.MomentumOptimizer(learning_rate=0.001, momentum=0.9).minimize(loss)
-    
+
 batchSize = 128
 with tf.Session(graph=graph) as session:
     session.run(tf.initialize_all_variables())
     saver = tf.train.Saver()
+    
     step = 0
     while (True):
         offset = (step * batchSize) % (trainingLabel.shape[0] - batchSize)
         # slicing the whole training set into batches # 
         batchData = trainingData[offset:(offset + batchSize),:,:,:]  
         batchLabel = trainingLabel[offset:(offset + batchSize),:]
+        
         _, stepLoss, stepAcc = session.run([optimizer, loss, accuracy], \
-                                    feed_dict={tfTrainingData: batchData, tfTrainingLabels: batchLabel, keep_prob: 0.5})
+                            feed_dict={tfTrainingData: batchData, tfTrainingLabels: batchLabel, keep_prob: 0.8})
         step += 1
         if (step == 1 or step % 100 == 0):
             validLoss, validAcc = session.run([loss, accuracy], \
                                     feed_dict={tfTrainingData: validateData[:1000,:,:,:], \
                                                tfTrainingLabels: validateLabel[:1000,:] , keep_prob: 1.0})
-            print ("Step %5d: Loss = %10.6f, TrainAcc = %10.6f, validLoss = %10.6f, validAcc = %10.6f" % (step, stepLoss, stepAcc, validLoss, validAcc))
-
-        if (step > 10000):
-            save_path = saver.save(session, "model/characterDetector_temp.ckpt", global_step=0)
-            tf.train.write_graph(session.graph_def, 'model', 'characterDetector_temp.pb', False)
-            freeze_graph.freeze_graph('model/characterDetector_temp.pb', '', True, \
-                                      'model/characterDetector_temp.ckpt-0', 'Output/softmax,Output/loss,Output/accuracy', \
-                                      'save/restore_all', 'save/Const:0', "model/characterDetector_prod.pb", False, "") 
-            os.system('rm model/characterDetector_temp.*')
-            break
+            print ("Step %5d: Loss = %10.6f, TrainAcc = %10.6f, validLoss = %10.6f, validAcc = %10.6f" % \
+                    (step, stepLoss, stepAcc, validLoss, validAcc))
+            
+            if (step > 90000):
+                save_path = saver.save(session, "model/characterClassification_temp.ckpt", global_step=0)
+                tf.train.write_graph(session.graph_def, 'model', 'characterClassification_temp.pb', False)
+                freeze_graph.freeze_graph('model/characterClassification_temp.pb', '', True, \
+                                      'model/characterClassification_temp.ckpt-0', 'Output/softmax,Output/loss,Output/accuracy', \
+                                      'save/restore_all', 'save/Const:0', "model/characterClassification_prod.pb", False, "") 
+                os.system('rm model/characterClassification_temp.*')
+                #save_path = saver.save(session, "bcd.ckpt")
+                break
 # ==========================================================================================================================
 # End of code
 # ==========================================================================================================================
