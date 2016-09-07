c187c8e393d8354c85621765fe72f4488c52d551
ZAZAZakari <tvbatvtv2@hotmail.com>
2016-09-07 06:52:25 +0000

    feat: use .mat instead of .pickle
    .mat file for saving data because the file is well-compressed and
    the output file is relatively small than .pickle file

--- a_dataPreprocessing.py
+++ a_dataPreprocessing.py
@@ -9,7 +9,7 @@
 #                    trainingLabel.pickle, validateLabel.pickle, testingLabel.pickle
 # ==========================================================================================================================
 import scipy.io
-import toPickle
+import maybe_download
 import numpy as np
 from sklearn.cross_validation import train_test_split
 
@@ -41,55 +41,68 @@ def oneHotEncoding(numUniqueLabels, labelList):
     for dataIndex, labelIndex in enumerate(labelList):	
         labelListEncoded[dataIndex][labelIndex] = 1.0
     return np.array(labelListEncoded, dtype=np.float32)
-	
+
 # ==========================================================================================================================
 # ==========================================================================================================================
 # ALGORITHM STARTS HERE
 # ==========================================================================================================================
 # [Step 1] Load raw dataset from the external .mat file 
 # ==========================================================================================================================
-trainingMat = scipy.io.loadmat('train_32x32.mat')
-testingMat = scipy.io.loadmat('test_32x32.mat')
+maybe_download.download('processed_data', 'https://storage.googleapis.com/yobi3d-deep-learning/raw/test_32x32.mat')
+maybe_download.download('processed_data', 'https://storage.googleapis.com/yobi3d-deep-learning/raw/train_32x32.mat')
+maybe_download.download('processed_data', 'https://storage.googleapis.com/yobi3d-deep-learning/raw/negative_samples.mat')
 
-# ==========================================================================================================================
-# [Step 2] Normalize the input training dataset by dividing each element by 255.0
-# ==========================================================================================================================
-trainingData = trainingMat['X'] / 255.0 
-trainingLabel = trainingMat['y']
-testingData = testingMat['X'] / 255.0
-testingLabel = testingMat['y']
-del trainingMat			# release resource
-del testingMat			# release resource
+trainingMat = scipy.io.loadmat('processed_data/train_32x32.mat')
+testingMat = scipy.io.loadmat('processed_data/test_32x32.mat')
+negativeSamples_data = scipy.io.loadmat('processed_data/negative_samples.mat')['data']
+negativeSamples_label = np.array([[11]] * negativeSamples_data.shape[0])
 
 # ==========================================================================================================================
 # [Step 3] Reshaping the dataset from (32, 32, 3, m) to (m, 32, 32, 3), to make it easier to split and train
 # ==========================================================================================================================
-trainingData = reshapeDataset(trainingData)
-testingData = reshapeDataset(testingData)
+trainingMat['X'] = reshapeDataset(trainingMat['X'])
+testingMat['X'] = reshapeDataset(testingMat['X'])
+
+
+trainingMat['X'] = np.concatenate((trainingMat['X'], testingMat['X'], negativeSamples_data), axis=0)
+trainingMat['y'] = np.concatenate((trainingMat['y'], testingMat['y'], negativeSamples_label), axis=0)
+del testingMat
+del negativeSamples_data
+del negativeSamples_label
+
+# ==========================================================================================================================
+# [Step 2] Normalize the input training dataset by dividing each element by 255.0
+# ==========================================================================================================================
+trainingMat['X'] = trainingMat['X'] / 255.0
+trainingMat['y'] = trainingMat['y'].flatten()-1
 
 # ==========================================================================================================================
 # [Step 4] Divide the whole training into two sets, 70% for training, 30% for validate, stratified to make sure 
 #          the number of data for each class is divided equally
 # ==========================================================================================================================
-trainingData, validateData, trainingLabel, validateLabel = train_test_split(trainingData, trainingLabel, test_size=0.3, stratify=trainingLabel)
+trainingData, validateData, trainingLabel, validateLabel = train_test_split(trainingMat['X'], trainingMat['y'][0], test_size=0.3, stratify=trainingMat['y'][0])
 
+print '222'
+del trainingMat
+print '333'
+validateData, testingData, validateLabel, testingLabel = train_test_split(validateData, validateLabel, test_size=0.5, stratify=validateLabel)
+
+
+print trainingData.shape
+print trainingLabel.shape
+print validateData.shape
+print validateLabel.shape
+print testingData.shape
+print testingLabel.shape
+
+'''
 # ==========================================================================================================================
 # [Step 5] One-hot encoding each label
 # ==========================================================================================================================
 trainingLabel = oneHotEncoding(10, trainingLabel.flatten()-1)
 validateLabel = oneHotEncoding(10, validateLabel.flatten()-1)
 testingLabel = oneHotEncoding(10, testingLabel.flatten()-1)
-
-# ==========================================================================================================================
-# [Step 6] Save all the data to external pickle files
-# ==========================================================================================================================
-toPickle.save(trainingData, 'trainingData', 'processed_data')
-toPickle.save(trainingLabel, 'trainingLabel', 'processed_data')
-toPickle.save(validateData, 'validateData', 'processed_data')
-toPickle.save(validateLabel, 'validateLabel', 'processed_data')
-toPickle.save(testingData, 'testingData', 'processed_data')
-toPickle.save(testingLabel, 'testingLabel', 'processed_data')
-
+'''
 # ==========================================================================================================================
 # End of code
 # ==========================================================================================================================
