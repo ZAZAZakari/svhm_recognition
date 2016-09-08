5cdf77e2e983d4a56596022eb83acf13559c0f89
ZAZAZakari <tvbatvtv2@hotmail.com>
2016-09-08 11:44:35 +0800

    style: adding comments on step a_dataPreprocessing
    add more comments on a_dataPreprocessing.py to make a clearer understanding
    of the flow of the code

--- a_dataPreprocessing.py
+++ a_dataPreprocessing.py
@@ -46,24 +46,25 @@ def oneHotEncoding(numUniqueLabels, labelList):
 # ==========================================================================================================================
 # ALGORITHM STARTS HERE
 # ==========================================================================================================================
-# [Step 1] Load raw dataset from the external .mat file 
+# [Step 0] Check if the needed dataset exists, if not, download from cloud
 # ==========================================================================================================================
 maybe_download.download('processed_data', 'https://storage.googleapis.com/yobi3d-deep-learning/raw/test_32x32.mat')
 maybe_download.download('processed_data', 'https://storage.googleapis.com/yobi3d-deep-learning/raw/train_32x32.mat')
 maybe_download.download('processed_data', 'https://storage.googleapis.com/yobi3d-deep-learning/raw/negative_samples.mat')
 
+# ==========================================================================================================================
+# [Step 1] Load raw dataset from the external .mat file 
+# ==========================================================================================================================
 trainingMat = scipy.io.loadmat('processed_data/train_32x32.mat')
 testingMat = scipy.io.loadmat('processed_data/test_32x32.mat')
 negativeSamples_data = scipy.io.loadmat('processed_data/negative_samples.mat')['data']
 negativeSamples_label = np.array([[11]] * negativeSamples_data.shape[0])
 
 # ==========================================================================================================================
-# [Step 3] Reshaping the dataset from (32, 32, 3, m) to (m, 32, 32, 3), to make it easier to split and train
+# [Step 2] Reshaping the dataset from (32, 32, 3, m) to (m, 32, 32, 3), to make it easier to split and train
 # ==========================================================================================================================
 trainingMat['X'] = reshapeDataset(trainingMat['X'])
 testingMat['X'] = reshapeDataset(testingMat['X'])
-
-
 trainingMat['X'] = np.concatenate((trainingMat['X'], testingMat['X'], negativeSamples_data), axis=0)
 trainingMat['y'] = np.concatenate((trainingMat['y'], testingMat['y'], negativeSamples_label), axis=0)
 del testingMat
@@ -71,7 +72,7 @@ del negativeSamples_data
 del negativeSamples_label
 
 # ==========================================================================================================================
-# [Step 2] Normalize the input training dataset by dividing each element by 255.0
+# [Step 3] Normalize the input training dataset by dividing each element by 255.0
 # ==========================================================================================================================
 trainingMat['X'] = trainingMat['X'] / 255.0
 trainingMat['y'] = trainingMat['y'].flatten()-1
@@ -80,29 +81,21 @@ trainingMat['y'] = trainingMat['y'].flatten()-1
 # [Step 4] Divide the whole training into two sets, 70% for training, 30% for validate, stratified to make sure 
 #          the number of data for each class is divided equally
 # ==========================================================================================================================
-trainingData, validateData, trainingLabel, validateLabel = train_test_split(trainingMat['X'], trainingMat['y'][0], test_size=0.3, stratify=trainingMat['y'][0])
-
-print '222'
+trainingData, validateData, trainingLabel, validateLabel = train_test_split(trainingMat['X'], trainingMat['y'], test_size=0.3, stratify=trainingMat['y'])
 del trainingMat
-print '333'
 validateData, testingData, validateLabel, testingLabel = train_test_split(validateData, validateLabel, test_size=0.5, stratify=validateLabel)
 
-
-print trainingData.shape
-print trainingLabel.shape
-print validateData.shape
-print validateLabel.shape
-print testingData.shape
-print testingLabel.shape
-
-'''
 # ==========================================================================================================================
-# [Step 5] One-hot encoding each label
+# [Step 5] Save the processed splited matrix to external .mat files
 # ==========================================================================================================================
-trainingLabel = oneHotEncoding(10, trainingLabel.flatten()-1)
-validateLabel = oneHotEncoding(10, validateLabel.flatten()-1)
-testingLabel = oneHotEncoding(10, testingLabel.flatten()-1)
-'''
+scipy.io.savemat('processed_data/training.mat', {'data': trainingData, 'label': oneHotEncoding(11, trainingLabel)})
+del trainingData, trainingLabel
+
+scipy.io.savemat('processed_data/validate.mat', {'data': validateData, 'label': oneHotEncoding(11, validateLabel)})
+del validateData, validateLabel
+
+scipy.io.savemat('processed_data/test.mat', {'data': testingData, 'label': oneHotEncoding(11, testingLabel)})
+del testingData, testingLabel
 # ==========================================================================================================================
 # End of code
 # ==========================================================================================================================
