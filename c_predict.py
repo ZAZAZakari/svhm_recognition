d17400ad8e07d7fee79ad6aa39fb0ed95fbcdb51
ZAZAZakari <tvbatvtv2@hotmail.com>
2016-09-15 08:17:21 +0000

    feat: integrate the text overlaying feature to the prediction result
    --- when making prediction, also draw a text overlaying the image.
    --- Remove the skeleton code (textOverlay.py) as it has been integrated
        to c_predict.py

--- c_predict.py
+++ c_predict.py
@@ -2,14 +2,18 @@ import numpy as np
 import tensorflow as tf
 import scipy.misc
 from tensorflow.python.platform import gfile
+from PIL import Image, ImageFont, ImageDraw
 
-inputImage = scipy.misc.imread('test.png')
 
-oneSnapshot = inputImage[0:32, 0:32, :] / 255.0
+inputImage = Image.open('2376.png')
+inputImageMatrix = np.array(inputImage)
+
+draw = ImageDraw.Draw(inputImage)
+font = ImageFont.truetype("Silom.ttf", 30)
 
 classifierName= 'streetNumberClassifier'
-imageWidth = inputImage.shape[0]
-imageHeight = inputImage.shape[1]
+imageWidth = inputImageMatrix.shape[0]
+imageHeight = inputImageMatrix.shape[1]
 
 with tf.Session() as session:   
   with gfile.FastGFile(("model/%s_prod.pb" % (classifierName)), "rb") as f:
@@ -18,21 +22,24 @@ with tf.Session() as session:
     session.graph.as_default()
     tf.import_graph_def(graph_def, name='')
     
+    '''
     output = session.graph.get_tensor_by_name("Output/softmax:0")
     prediction = session.run(output, {'tfTrainingData:0': oneSnapshot[None, :, :, :], 'keep_prob:0': 1.0})
     print prediction
     print np.argmax(prediction[0])
     '''
-    for i in range(0, imageWidth-32, 10):
-      for j in range(0, imageHeight-32, 10):
-        partialImage = inputImage[i:i+32, j:j+32, :] / 255.0
+    
+    for i in range(0, imageWidth-32, 32):
+      for j in range(0, imageHeight-32, 20):
+        partialImage = inputImageMatrix[i:i+32, j:j+32, :] / 255.0
         partialImage = partialImage[None, :, :, :]
-        output = persisted_sess.graph.get_tensor_by_name("Output/softmax:0")
-        prediction = persisted_sess.run(output, {'tfTrainingData:0': partialImage, 'keep_prob:0': 1.0})
-        print inputImage[i:i+32, j:j+32, :]
-        scipy.misc.imsave(('outfile%d-%d.jpg' % (i,j)), inputImage[i:i+32, j:j+32, :])
-        print i, j, '=========================', np.argmax(prediction[0])
-    '''
+        output = session.graph.get_tensor_by_name("Output/softmax:0")
+        prediction = session.run(output, {'tfTrainingData:0': partialImage, 'keep_prob:0': 1.0})
+        draw.rectangle((j, i, j+20, i+32), fill='black')
+        draw.text((j, i), str(np.argmax(prediction)+1),(255,255,255), font=font)
+        print i, j, '===================', np.argmax(prediction)
+        scipy.misc.imsave(('outfile%d-%d.png' % (i,j)), inputImageMatrix[i:i+32, j:j+32, :])
+    inputImage.save('sample-out.png')
 # ==========================================================================================================================
 # End of code
 # ==========================================================================================================================
